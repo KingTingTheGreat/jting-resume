ccbd6f0b6b768501d4772f221e651ba1ce896bcd
Jeffrey <kingtingthegreat@gmail.com>
2025-01-08 22:32:06 -0500

    move views to public

--- render/render.go
+++ render/render.go
@@ -14,7 +14,7 @@ var funcMap = template.FuncMap{
 
 func RenderPage(w io.Writer, data interface{}) {
 	tmpl := template.New("").Funcs(funcMap)
-	tmpl = template.Must(tmpl.ParseGlob("views/*.html"))
+	tmpl = template.Must(tmpl.ParseGlob("public/views/*.html"))
 
 	err := tmpl.ExecuteTemplate(w, "index", data)
 	if err != nil {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/*.html"],
+  content: ["./public/views/*.html"],
   theme: {
     extend: {
       fontSize: {
